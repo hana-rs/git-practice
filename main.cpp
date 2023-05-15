10e7289fd08d0fc012ad5303ccb63e22755564fe
hana <hanahanabi87@icloud.com>
2023-05-15 18:39:26 +0900

    足し算の関数を追加

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
- #include <bits/stdc++.h>
- using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
- int main() {
-    cout << "Hello Maximum!" << endl;
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << add(3, -2) << endl;
     return 0;
- }
+}
